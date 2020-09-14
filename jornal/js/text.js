dca14236f0ac9c4e77b36e1018ac32ff9904b338
Ahmed5510-Mac <54468849+Ahmed5510-Mac@users.noreply.github.com>
2020-09-14 12:33:41 +0200

    the project test

---
+++ jornal/js/text.js
@@ -0,0 +1,95 @@
+// var price=1200;
+// var usernam=Ahmed Darwish;
+// var p_code=tyvb525;
+// var code=231561561;
+// var is_admin=true;
+// var login=false;
+// var x=null;
+// window.alert(typeof(x));
+//  //---------------------------------
+// var enterusernam=prompt("enter your name");
+// window.alert("wlcome to" + enterusernam)
+
+// //------------------------------
+// var num1 =prompt("Enter num 1");
+// var num2 =prompt("Enter num 2");
+// var result = Number( num1)+Number(num2)
+// window.alert(result)
+// //-----------------------
+//*-------------start slider------------------
+// $(document).ready(function () {
+
+//     $("#sidebar").mCustomScrollbar({
+//          theme: "minimal"
+//     });
+
+//     $('#sidebarCollapse').on('click', function () {
+//         $('#sidebar').toggleClass('active');
+//     });
+
+// });
+
+
+//*-------------End slider------------------
+// function getsum(x,y)
+// {
+//     var result=x+y;
+//     return result
+// }
+// faunction calAvg(num1,num2)
+// {
+//     var sum=num1+num2;
+//     var result=sum/2;
+//     return result;
+// }
+// var r1 =getsum(10,90);
+// var r2 = getsum(100,50);
+// var finalresult =calAvg(r1,r2)
+// console.log(finalresult);
+//-----------------------------
+// sum(10,5)
+// funcation sum(x,t);
+// {
+//     console.log(result)
+//     var result =x+yield;
+//}
+// -------------------------
+// var sum =function(c,g)
+// {
+//     var result =x+yield;
+//     return result;
+// }
+//----------------------------------
+// var product =
+// {
+//   name:'ahmed',
+//   salary:90000,
+//   marrid:false,
+//   age:27,
+//   component:{audio:'9000 amp',led :'screen',spaker:'9062w'},
+//   powerOn:function()
+//   {
+
+//   }
+// }
+// console.log(product.salary,product.age,product.marrid);
+
+        // var user=
+        // {
+        //     name   :"ahmed",
+        //     age    : 26,
+        //     Gender : 'Mail',
+        //     status : 'sengel',
+        //     logIn  :function log(paowrd, email)
+        //     {
+        //         console.log('welcom Back');
+        //     }
+        // }
+        // console.log(user.name);
+
+        // ===================================================================
+        // ===================================================================
+        // ===================================================================
+        // ===================================================================
+        // ===================================================================
+        document.getElementById("falon").innerHTML="hello javsscript";
